1c1033c4b79275cac2424fa3d523de2beda8bbc8
LOIC PETIT <loicpetidev@gmail.com>
2017-08-01 13:48:27 +0200

    Add response status code OK matcher

--- rest-server/app/test/helpers/matchers-helper.js
+++ rest-server/app/test/helpers/matchers-helper.js
@@ -19,8 +19,28 @@ beforeEach(function () {
         }
     }
 
+    function toBeStatusOk(util, customEqualityTesters) {
+        return {
+            compare: function (actual, expected) {
+                //  result
+                var result = {
+                    pass: actual.statusCode === 200
+                };
+                //  error messages
+                if (result.pass) {
+                    result.message = 'Expected ' + actual.statusCode + ' not to equal 200';
+                } else {
+                    result.message = 'Expected ' + actual.statusCode + ' to equal 200';
+                }
+                
+                return result
+            }
+        }
+    }
+
     jasmine.addMatchers({
-        toContainsString: toContainsString
+        toContainsString: toContainsString,
+        toBeStatusOk: toBeStatusOk
     });
 
 });
